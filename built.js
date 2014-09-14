2f95167457bfede7151a2976bb0d9afdc9a5ee1e
JeanMichel FRANCOIS <toutpt@gmail.com>
2014-09-14 15:49:04 +0200

    up

--- built.js
+++ built.js
@@ -644,11 +644,12 @@ angular.module('osmMobileTagIt').directive('tagsTable', function(){
 angular.module('osmMobileTagIt.services').factory('tagsService',
     ['$q', 'osmTagInfoAPI', function($q, osmTagInfoAPI){
         return {
-            '_cached': {},
-            get: function(key){
+            '_cachedKeyValues': {},
+            '_cachedKeyWikiPages': {},
+            getKeyValues: function(key){
                 var deferred = $q.defer();
                 var self = this;
-                if (self._cached[key] === undefined){
+                if (self._cachedKeyValues[key] === undefined){
                     osmTagInfoAPI.getKeyValues({
                         //key=shop&lang=fr&sortname=count&sortorder=desc&page=1&rp=400&format=json_pretty
                         key:key,
@@ -658,13 +659,31 @@ angular.module('osmMobileTagIt.services').factory('tagsService',
                         page:1,
                         rp:100
                     }).then(function(data){
-                        self._cached[key] = data.data;
-                        deferred.resolve(self._cached[key]);
+                        self._cachedKeyValues[key] = data.data;
+                        deferred.resolve(self._cachedKeyValues[key]);
                     }, function(error){
                         deferred.reject(error);
                     });
                 }else{
-                    deferred.resolve(self._cached[key]);
+                    deferred.resolve(self._cachedKeyValues[key]);
+                }
+                return deferred.promise;
+            },
+            getKeyWikiPages: function(key){
+                var deferred = $q.defer();
+                var self = this;
+                if (self._cachedKeyWikiPages[key] === undefined){
+                    osmTagInfoAPI.getKeyWikiPages({
+                        //key=shop&lang=fr&sortname=count&sortorder=desc&page=1&rp=400&format=json_pretty
+                        key:key
+                    }).then(function(wikipages){
+                        self._cachedKeyWikiPages[key] = wikipages;
+                        deferred.resolve(self._cachedKeyWikiPages[key]);
+                    }, function(error){
+                        deferred.reject(error);
+                    });
+                }else{
+                    deferred.resolve(self._cachedKeyWikiPages[key]);
                 }
                 return deferred.promise;
             }
@@ -679,6 +698,7 @@ angular.module('osmMobileTagIt.controllers').controller('TagsTableController',
         $scope.newTagKey = '';
         $scope.newTagValue = '';
         $scope.tagValues = {};
+        $scope.tagWikiPages = {};
         $scope.addTag = function(){
             if ($scope.newTagKey && $scope.newTagValue){
                 $scope.tags[$scope.newTagKey] = $scope.newTagValue;
@@ -693,14 +713,32 @@ angular.module('osmMobileTagIt.controllers').controller('TagsTableController',
                 $scope.tagValues[k] = values;
             });*/
         };
-        $scope.loadTagValues = function(tags){
-            for (var k in tags) {
-                if ($scope.tagValues[k] === undefined && k.indexOf(':')===-1 && k!== 'source' && k!== 'name'){
-                    console.log('load tags for ' +k);
-                    tagsService.get(k).then(function(values){
-                        $scope.tagValues[k] = values;
-                    });
-                }
+        $scope.toggleTagInfo = function(k){
+            if ($scope.tagWikiPages[k] !== undefined){
+                $scope.tagWikiPages[k] = undefined;
+            }else{
+                var getWikiPageByLang = function(wikipages, lang){
+                    for (var i = 0; i < wikipages.length; i++) {
+                        if (wikipages[i].lang === lang){
+                            return wikipages[i];
+                        }
+                    }
+                };
+                tagsService.getKeyWikiPages(k).then(function(wikipages){
+                    if ($scope.tagWikiPages[k]===undefined){
+                        $scope.tagWikiPages[k] = getWikiPageByLang(wikipages, 'fr');
+                        if ($scope.tagWikiPages[k]===undefined){
+                            $scope.tagWikiPages[k] = getWikiPageByLang(wikipages, 'en');
+                        }
+                    }
+                });
+            }
+            if ($scope.tagValues[k] !== undefined){
+                $scope.tagValues[k] = undefined;
+            }else{
+                tagsService.getKeyValues(k).then(function(values){
+                    $scope.tagValues[k] = values;
+                });
             }
         };
     }]
